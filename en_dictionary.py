764e4e69298f53921175a290c96a4d859c74d136
Anderson Alcará Alvarenga <anderalvarenga@gmail.com>
2025-02-04 13:52:20 -0300

    remove pydictionary and uses nltk in place

--- en_dictionary.py
+++ en_dictionary.py
@@ -1,15 +1,35 @@
-from PyDictionary import PyDictionary
+import requests
+from nltk.corpus import wordnet
 
+POS_TAGS = {
+    "n": "Noun",
+    "v": "Verb",
+    "a": "Adjective",
+    "s": "Adjective (satellite)",
+    "r": "Adverb"
+}
 
-def get_meaning(string):
-    dictionary = PyDictionary()
-    meaning = dictionary.meaning(string)
+def get_meaning(word):
+    
+    synsets = wordnet.synsets(word)
+    print (f"synsets: {synsets}")
+    if synsets:
+        formated_meaning = ""
+        for synset in synsets:
+            print (f"synset: {synset}")
+            pos = POS_TAGS.get(synset.pos(), "Other")
+            definition = synset.definition()
+            formated_meaning += f"<i>{pos}</i><br />\t- {definition}<br /><br />"
+        return formated_meaning
 
-    formated_meaning = ""
-    for word_class in meaning:
-        formated_meaning += f"<i>{word_class}</i><br />"
-        for item in meaning[word_class]:
-            formated_meaning += f"\t- {item}<br />"
-        formated_meaning += "<br />"
+    url = f"https://api.urbandictionary.com/v0/define?term={word}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        if data["list"]:
+            formated_meaning = "<i>Urban Dictionary</i><br />"
+            for entry in data["list"][:3]:
+                formated_meaning += f"\t- {entry['definition']}<br /><br />"
+            return formated_meaning
 
-    return formated_meaning
+    return
